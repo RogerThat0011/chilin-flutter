0d96f59bcb4d92bb59b52c6058e9cba3b3fb9f9f
Carlos-Castellanos24 <carloscastellanostrabajo@outlook.com>
2024-03-06 16:53:51 -0600

    Agregando conexion a firebase

--- cliente_pupuseria_chilin/lib/main.dart
+++ cliente_pupuseria_chilin/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:t_store/app.dart';
+import 'package:t_store/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  //INICIALIZAR FIREBASE
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const App());
 }
