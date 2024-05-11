f69ac345211d054d2a1c3434cc10d68064fa9186
diego <diegoecheverria821@gmail.com>
2024-05-10 23:10:20 -0600

    Cambios en Splash

--- cliente_pupuseria_chilin/lib/features/screens/onboarding/widgets/onboarding_dot_navigation.dart
+++ cliente_pupuseria_chilin/lib/features/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -24,7 +24,7 @@ class OnBoardingDotNavigation extends StatelessWidget {
       child: SmoothPageIndicator(
           controller: controller.pageController,
           onDotClicked: controller.dotNavigationClick,
-          count: 3,
+          count: 4,
           effect: ExpandingDotsEffect(
               activeDotColor: dark ? TColors.light : TColors.dark, dotHeight: 6)),
     );
